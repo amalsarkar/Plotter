80f88f257e5513e351605899b547b510d33d43a0
masegura <manuel.segura@cern.ch>
2017-11-12 06:22:30 -0600

    First Commit

---
+++ run_plotter.sh
@@ -0,0 +1,4 @@
+echo --------------- Deleting old root files ----------------------
+rm *.root
+echo ---------------- Running Plotter ------ ---------------------
+./Plotter  config/2017-stau.config
